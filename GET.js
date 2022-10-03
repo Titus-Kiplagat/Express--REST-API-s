ecba2b2951e5cecab39159757c1249b8e40bc658
Titus Lagat <lagattyto@gmail.com>
2022-10-03 09:38:43 +0300

    get request

---
+++ GET.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const courses = [
+	{id: 1, course: 'Node'},
+	{id: 2, course: 'React'},
+	{id: 3, course: 'Javascript'}
+]
+
+app.get('/api/courses', (req, res) => {
+	res.send(courses);
+})
+
+app.get('/api/courses/:id', (req, res) => {
+	const course = courses.find(course => course.id === parseInt(req.params.id))
+	if (!course) res.status(404).send('The course with the given ID was not found!')
+	res.send(course);
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}....`));
